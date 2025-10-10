81c912d0623d43a0cbf92a776a030b11472da1b6
Alexander Richard <95628434+alexrichrd@users.noreply.github.com>
2025-10-10 22:06:32 +0200

    Add Position class

---
+++ src/Position.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <memory>
+
+namespace chess {
+
+class Piece;
+
+class Position {
+  // chess positions are internally represented as array positions,
+  // e.g. position c4 is 2,3 (column, row)
+  unsigned row;
+  unsigned column;
+  std::unique_ptr<Piece> piece_ptr;
+
+ public:
+  Position() = default;
+  Position(unsigned row, unsigned column, std::unique_ptr<Piece> piece_ptr)
+      : row(row), column(column), piece_ptr(std::move(piece_ptr)) {};
+  Position(const Position&) = delete;
+  Position& operator=(const Position& other) = delete;
+  Position& operator=(Position&& other) noexcept {
+    if (this == &other) {
+      return *this;
+    }
+    row = other.row;
+    column = other.column;
+    piece_ptr = std::move(other.piece_ptr);
+    return *this;
+  }
+  Position(Position&& other) noexcept
+      : row(other.row),
+        column(other.column),
+        piece_ptr(std::move(other.piece_ptr)) {}
+
+  unsigned get_row() { return row; }
+  unsigned get_column() { return column; }
+  Piece* get_piece_ptr() { return piece_ptr.get(); }
+  std::unique_ptr<Piece> release_piece_ptr() { return std::move(piece_ptr); }
+  void set_piece_ptr(std::unique_ptr<Piece> piece_ptr_) {
+    piece_ptr = std::move(piece_ptr_);
+  }
+};
+
+}  // namespace chess
